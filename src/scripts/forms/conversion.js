f6b1443c2e641af953d579bd1273a43bc9377eb4
Vincent Köckerling <v.koeckerling@icloud.com>
2024-10-31 16:16:23 +0100

    Conversion: Format and use onReady

--- src/scripts/forms/conversion.js
+++ src/scripts/forms/conversion.js
@@ -1,105 +1,116 @@
-document.addEventListener("DOMContentLoaded", () => {
-	if (window.location.search) {
-		const params = new URLSearchParams(window.location.search);
+import { onReady } from "../../utils/on_ready";
 
-		const gclid = params.get("gclid");
-		if (gclid) {
-			window.localStorage.setItem("gclid", gclid);
-		}
+onReady(() => {
+  if (window.location.search) {
+    const params = new URLSearchParams(window.location.search);
 
-		const fbclid = params.get("fbclid");
-		if (fbclid) {
-			const fbc = `fb.1.${Date.now()}.${fbclid}`;
-			window.localStorage.setItem("fbc", fbc);
-		}
+    const gclid = params.get("gclid");
+    if (gclid) {
+      window.localStorage.setItem("gclid", gclid);
+    }
 
-		const ttclid = params.get("ttclid");
-		if (ttclid) {
-			window.localStorage.setItem("ttclid", ttclid);
-		}
-	}
+    const fbclid = params.get("fbclid");
+    if (fbclid) {
+      const fbc = `fb.1.${Date.now()}.${fbclid}`;
+      window.localStorage.setItem("fbc", fbc);
+    }
 
-	const forms = Array.from(document.querySelectorAll("[c-conversion] > form"));
-	forms.forEach((form) => {
-		// Create hidden input for each and store it in object
-		const inputs = ["gclid", "fbc", "fbp", "user-agent", "ttclid", "url"].reduce((obj, name) => {
-			const input = document.createElement("input");
-			input.type = "hidden";
-			input.name = name;
-			form.appendChild(input);
+    const ttclid = params.get("ttclid");
+    if (ttclid) {
+      window.localStorage.setItem("ttclid", ttclid);
+    }
+  }
 
-			return {
-				...obj,
-				[name]: input,
-			};
-		}, {});
+  const forms = Array.from(document.querySelectorAll("[c-conversion] > form"));
+  forms.forEach((form) => {
+    // Create hidden input for each and store it in object
+    const inputs = [
+      "gclid",
+      "fbc",
+      "fbp",
+      "user-agent",
+      "ttclid",
+      "url",
+    ].reduce((obj, name) => {
+      const input = document.createElement("input");
+      input.type = "hidden";
+      input.name = name;
+      form.appendChild(input);
 
-		form.addEventListener("submit", () => {
-			const conversionIds = getConversionIDs();
+      return {
+        ...obj,
+        [name]: input,
+      };
+    }, {});
 
-			inputs.gclid.value = conversionIds.gclid;
-			inputs.fbc.value = conversionIds.fbc;
-			inputs.fbp.value = conversionIds.fbp;
-			inputs["user-agent"].value = conversionIds.useragent;
-			inputs.ttclid.value = conversionIds.ttclid;
-			inputs.url.value = conversionIds.url;
+    form.addEventListener("submit", () => {
+      const conversionIds = getConversionIDs();
 
-			// Tracking Events
-			if (window.fbq !== undefined) {
-				fbq("track", "SubmitApplication", {}, { eventID: conversionIds.fbp });
-			}
-		});
-	});
+      inputs.gclid.value = conversionIds.gclid;
+      inputs.fbc.value = conversionIds.fbc;
+      inputs.fbp.value = conversionIds.fbp;
+      inputs["user-agent"].value = conversionIds.useragent;
+      inputs.ttclid.value = conversionIds.ttclid;
+      inputs.url.value = conversionIds.url;
 
-	const trackElements = Array.from(document.querySelectorAll("[data-fb-track]"));
-	trackElements.forEach((el) => {
-		el.addEventListener("click", () => {
-			const isDisabled = el.dataset.trackDisabled === "true";
-			if (isDisabled) return;
+      // Tracking Events
+      if (window.fbq !== undefined) {
+        fbq("track", "SubmitApplication", {}, { eventID: conversionIds.fbp });
+      }
+    });
+  });
 
-			const event = el.dataset.fbTrack;
-			const conversionIds = getConversionIDs();
-			const url = el.dataset.trackUrl;
+  const trackElements = Array.from(
+    document.querySelectorAll("[data-fb-track]"),
+  );
+  trackElements.forEach((el) => {
+    el.addEventListener("click", () => {
+      const isDisabled = el.dataset.trackDisabled === "true";
+      if (isDisabled) return;
 
-			if (window.fbq !== undefined) {
-				fbq("track", event, {}, { eventID: conversionIds.fbp });
-			}
+      const event = el.dataset.fbTrack;
+      const conversionIds = getConversionIDs();
+      const url = el.dataset.trackUrl;
 
-			fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					event,
-					...conversionIds,
-				}),
-			});
-		});
-	});
+      if (window.fbq !== undefined) {
+        fbq("track", event, {}, { eventID: conversionIds.fbp });
+      }
 
-	function getConversionIDs() {
-		const gclid = window.localStorage.getItem("gclid");
-		const fbc = window.localStorage.getItem("fbc");
-		const fbp = getCookie("_fbp");
-		const useragent = navigator.userAgent;
-		const ttclid = window.localStorage.getItem("ttclid");
-		const url = window.location.href;
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          event,
+          ...conversionIds,
+        }),
+      });
+    });
+  });
 
-		return {
-			gclid,
-			fbc,
-			fbp,
-			useragent,
-			ttclid,
-			url,
-		};
-	}
+  function getConversionIDs() {
+    const gclid = window.localStorage.getItem("gclid");
+    const fbc = window.localStorage.getItem("fbc");
+    const fbp = getCookie("_fbp");
+    const useragent = navigator.userAgent;
+    const ttclid = window.localStorage.getItem("ttclid");
+    const url = window.location.href;
 
-	function getCookie(name) {
-		const value = `; ${document.cookie}`;
-		const parts = value.split(`; ${name}=`);
-		if (parts.length === 2) return parts.pop().split(";").shift();
-		else return null;
-	}
+    return {
+      gclid,
+      fbc,
+      fbp,
+      useragent,
+      ttclid,
+      url,
+    };
+  }
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+    else return null;
+  }
 });
